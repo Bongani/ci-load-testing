9c79fcad874739afdd969bae4cfeb502711ca8ef
Bongani Shongwe <bongani.s.shongwe@gmail.com>
2019-07-15 14:46:38 +0200

    Test fail portion

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import { Rate } from "k6/metrics";
+
+var myFailRate = new Rate("failed requests");
 
 export let options = {
   vus: 10,
-  duration: "30s"
+  duration: "30s",
+  thresholds: {
+      "failed requests": ["rate<0.1"], // <10% errors
+    }
 };
 
+export let errorRate = new Rate("errors");
+
 export default function() {
-  let res = http.get("http://test.loadimpact.com");
+  let res = http.get("https://loadimpact.com");
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 200
-  });
+  }) && myFailRate.add(2);
   sleep(1);
 };
